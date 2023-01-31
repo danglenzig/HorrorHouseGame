f4efc4554d28073593cc9a9e5ad9b260aefcbe25
PHallin <petter-hallin99@live.se>
2023-01-31 11:42:19 +0100

    Imported Camera Scripts and formatted them

---
+++ Assets/Scripts/CameraTriggerScript.cs
@@ -0,0 +1,17 @@
+using Cinemachine;
+using GameConstants;
+using UnityEngine;
+
+public class CameraTriggerScript : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera triggerCamera;
+    [SerializeField] private bool useTriggerStay;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(Tags.PlayerTag))
+        {
+            CameraManagerScript.CurrentActiveCamera = triggerCamera;
+        }
+    }
+}
