b85fc5a6841ef3e4f2ca0da391cd623134055e7c
PHallin <petter-hallin99@live.se>
2023-01-31 12:52:06 +0100

    Start of Implementation of Interaction System

---
+++ Assets/Scripts/GhostInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+public class GhostInteraction : MonoBehaviour, IPickupInteraction
+{
+    private Interactable interactedObject;
+    private List<Interactable> possibleInteractables;
+
+    public void AddInteractable(Interactable interactable)
+    {
+        possibleInteractables.Add(interactable);
+    }
+
+    public void RemoveInteractable(Interactable interactable)
+    {
+        possibleInteractables.Remove(interactable);
+    }
+
+    public void StopInteract()
+    {
+        interactedObject = null;
+    }
+}
+
+public interface IPickupInteraction
+{
+    void AddInteractable(Interactable interactable);
+    void StopInteract();
+}
+
+public interface ISimpleInteraction
+{
+    void Interact();
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using GameConstants;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private Collider interactableTrigger;
+    private InteractableState interactableState;
+    private bool isEnabled;
+    private Transform ownerTransform;
+
+    private void Start()
+    {
+        MakeTrigger();
+    }
+
+    private void Update()
+    {
+        if (ownerTransform != null && interactableState != InteractableState.Used)
+        {
+            FollowOwner();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isEnabled)
+        {
+            return;
+        }
+
+        if (!other.CompareTag(Tags.PlayerTag) || !other.TryGetComponent<GhostInteraction>(out var interactable))
+        {
+            return;
+        }
+
+        // TODO: Show input that interaction possible
+        interactable.AddInteractable(this);
+        ownerTransform = interactable.transform;
+        interactableState = InteractableState.Interacted;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!isEnabled)
+        {
+            return;
+        }
+
+        if (other.CompareTag(Tags.PlayerTag))
+        {
+            
+        }
+    }
+
+    private void FollowOwner()
+    {
+        transform.position = ownerTransform.position;
+    }
+
+    private void MakeTrigger()
+    {
+        interactableTrigger.isTrigger = true;
+    }
+
+    public void ToggleCollider()
+    {
+        isEnabled = !isEnabled;
+    }
+}
+
+internal enum InteractableState
+{
+    Free,
+    Interacted,
+    Used
+}
